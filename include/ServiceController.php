12a3242d60308ef7262403ce6a48e9baaf68ffca
Youssef Dahar <youssef.dahar@gmail.com>
2016-11-15 08:54:44 +0000

    Create ServiceController.php

---
+++ include/ServiceController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ydahar
+ * Date: 14/11/2016
+ * Time: 08:17
+ */
+
+ 
+function getAllServices() {
+    $serviceService = new ServiceService();
+    $response = $serviceService->getServices();
+    $response['error'] = false;
+    echoResponse(200, $response);
+}
+
+function getService($user_id) {
+    $serviceService = new ServiceService();
+    $response = $serviceService->getServiceByID($user_id);
+    $response['error'] = false;
+    echoResponse(200, $response);
+}
+
+
+function createService() {
+    verifyRequiredParams(array('nom','prenom','email','sexe', 'password'));
+    $personneService = new PersonneService();
+    $app = \Slim\Slim::getInstance();
+
+    $nom = $app->request->post('nom');
+    $prenom = $app->request->post('prenom');
+    $sexe = $app->request->post('sexe');
+    $password = $app->request->post('password');
+    $email = $app->request->post('email');
+    $tel = $app->request->post('tel');
+
+
+
+    $response = $personneService->createPersonne($nom,$prenom,$email,$tel,$password,$sexe);
+
+    echoResponse(200, $response);
+}
+
